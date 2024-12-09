89db3aff0a1231a2ddda73c8429e5d6067b0c375
Tobe642 <c2088748@hallam.shu.ac.uk>
2024-12-09 23:33:19 +0000

    Added registration app with model linked to Modules and Student

---
+++ registrations/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from students.models import Student
+from modules.models import Module
+
+
+class Registration(models.Model):
+        
+    student = models.ForeignKey(Student,on_delete=models.CASCADE, blank=False, null=False)
+    module = models.ForeignKey(Module,on_delete=models.CASCADE, blank=False, null=False)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.student} {self.module.name}'
+
+# Create your models here.
