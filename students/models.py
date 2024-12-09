4bdc1fbdbee3dc009ddfcabc8408ee1df06af251
Tobe642 <c2088748@hallam.shu.ac.uk>
2024-12-09 23:06:47 +0000

    Added Student app with model extending django auth user model

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True) 
+    image = models.ImageField(default='media/profile_pics/default.png', upload_to='profile_pics')
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
