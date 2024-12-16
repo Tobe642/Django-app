ec6e0b79cd460a13c2ee0c23a83671e6aeb5a48b
Tobe642 <c2088748@hallam.shu.ac.uk>
2024-12-16 20:37:49 +0000

    renames categories for django admin

--- categories/models.py
+++ categories/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return f'{self.name}'
