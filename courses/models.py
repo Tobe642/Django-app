10e97c07c9dd85c1bc783d10c551648e764f13c1
Tobe642 <c2088748@hallam.shu.ac.uk>
2025-01-09 21:41:47 +0000

    added additional model for extra features

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 #here
+
+#here
 class Course(models.Model):
         
     name = models.CharField(max_length=255, blank=False, null=False)
